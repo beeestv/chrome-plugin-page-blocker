b25fd3510c6d13623c9cd2940dd8560ad550a4eb
HuZhiwei <huzw@silvrr.com>
2017-05-27 18:53:46 +0800

    popup实现黑名单、提示语、开关设置

--- background.js
+++ background.js
@@ -1,95 +1,95 @@
-var blackList=[
-	'weibo',
-	'v2ex',
-	'qzone',
-	'tieba.baidu.com'
-	];
 var warningId = 'notification.warning';
 
 function hideWarning(done) {
-    chrome.notifications.clear(warningId, function() {
+    chrome.notifications.clear(warningId, function () {
         if (done) done();
     });
 }
 
 function showWarning() {
-    hideWarning(function() {
+    hideWarning(function () {
         chrome.notifications.create(warningId, {
             title: 'Page Blocker',
             type: 'basic',
             message: '滚去工作啊！！',
-            buttons: [{ title: 'Learn More' }],
+            buttons: [{title: 'Learn More'}],
             isClickable: true,
             priority: 2,
-        }, function() {});
+        }, function () {
+        });
     });
 }
 
-function doBlock(url) {
-	for (index in blackList) {
-		if (url.indexOf(blackList[index])>-1) {
-			if (time_range('12:30', '14:00') || time_range('19:20', '23:59')) {
-				alert('工作之余');
-				return false;
-			}
-			return true;
-		}
-	}
-	return false;
+function doBlock(url, callback) {
+    chrome.storage.local.get('blackList', function (items) {
+        var blackList = items.blackList;
+        if (blackList != null) {
+            for (index in blackList) {
+                if (url.indexOf(blackList[index]) > -1) {
+                    if (!time_range('12:30', '14:00') && !time_range('19:20', '23:59')) {
+                        callback();
+                        return;
+                    }
+                }
+            }
+        } else {
+            callback();
+        }
+    })
 }
 
 function execute(tabId, url) {
-	chrome.tabs.remove(tabId, function(){
+    chrome.tabs.remove(tabId, function () {
         chrome.storage.local.get('alertText', function (items) {
-			alert(items.alertText ? items.alertText : "滚去工作啊！！！");
+            alert(items.alertText ? items.alertText : "滚去工作啊！！！");
         })
-	});
+    });
 }
 
 function time_range(beginTime, endTime) {
-    var strb = beginTime.split (":");
+    var strb = beginTime.split(":");
     if (strb.length != 2) {
         return false;
     }
 
-    var stre = endTime.split (":");
+    var stre = endTime.split(":");
     if (stre.length != 2) {
         return false;
     }
 
-    var b = new Date ();
-    var e = new Date ();
-    var n = new Date ();
+    var b = new Date();
+    var e = new Date();
+    var n = new Date();
 
-    b.setHours (strb[0]);
-    b.setMinutes (strb[1]);
-    e.setHours (stre[0]);
-    e.setMinutes (stre[1]);
+    b.setHours(strb[0]);
+    b.setMinutes(strb[1]);
+    e.setHours(stre[0]);
+    e.setMinutes(stre[1]);
 
-    if (n.getTime () - b.getTime () > 0 && n.getTime () - e.getTime () < 0) {
+    if (n.getTime() - b.getTime() > 0 && n.getTime() - e.getTime() < 0) {
         return true;
     } else {
         return false;
     }
 }
 
-chrome.storage.local.get({"enable":true}, function(items){
-    chrome.browserAction.setIcon({path:"icon-" + items.enable + ".png"});
+chrome.storage.local.get({"enable": true}, function (items) {
+    chrome.browserAction.setIcon({path: "icon-" + items.enable + ".png"});
 });
 
-chrome.tabs.onCreated.addListener(function(tab) {
-    chrome.storage.local.get({"enable":true}, function(items){
+chrome.tabs.onCreated.addListener(function (tab) {
+    chrome.storage.local.get({"enable": true}, function (items) {
         if (!item.enable) return;
-        if (doBlock(tab.url)) {
+        doBlock(tab.url, function () {
             execute(tab.id, tab.url);
-        }
+        });
     });
 });
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    chrome.storage.local.get({"enable":true}, function(items){
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    chrome.storage.local.get({"enable": true}, function (items) {
         if (!items.enable) return;
-        if (doBlock(tab.url)) {
-            execute(tabId, tab.url);
-        }
+        doBlock(tab.url, function () {
+            execute(tab.id, tab.url);
+        });
     });
 });
--- popup.js
+++ popup.js
@@ -4,19 +4,28 @@
 var storage = chrome.storage.local;
 
 storage.get("enable", function (items) {
-    $('#onoff').prop('checked', items.enable);
+    $('#on-off').prop('checked', items.enable);
 });
 storage.get("alertText", function (items) {
     $('#alert-text').val(items.alertText);
 });
+storage.get('blackList', function (items) {
+    $('#black-list').val(items.blackList.join('\n'));
+});
 $(function () {
-    $('#onoff').click(function () {
-        var enable = $('#onoff').prop("checked");
+    $('#on-off').click(function () {
+        var enable = $('#on-off').prop("checked");
         chrome.browserAction.setIcon({path:"icon-" + enable + ".png"});
         storage.set({'enable':enable}, function () {});
     })
 
     $('#alert-text').change(function () {
         storage.set({'alertText': $('#alert-text').val()}, function () {});
+    });
+
+    $('#black-list').change(function () {
+        var blackListString = $('#black-list').val();
+        var blackList = blackListString.split('\n')
+        storage.set({'blackList':blackList}, function () {})
     });
 });
