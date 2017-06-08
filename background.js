13eccf10abf19f19b7be87b45e4fb6a92d2b8bb1
bestv <faultymanhzw@163.com>
2017-06-08 21:35:18 +0800

    [优化]增加课余时间段：0:00-9:30

--- background.js
+++ background.js
@@ -29,7 +29,7 @@ function doBlock(url, callback) {
                 var blackUrl = blackList[index].split(":")[0];
                 alertMessage = blackList[index].split(":")[1];
                 if (url.indexOf(blackUrl) > -1) {
-                    if (!time_range('12:30', '14:00') && !time_range('19:20', '23:59')) {
+                    if (!time_range('12:30', '14:00') && !time_range('19:20', '23:59') && !time_range('00:00', '09:30')) {
                         callback();
                         return;
                     }
